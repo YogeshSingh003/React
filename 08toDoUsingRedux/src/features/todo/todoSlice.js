69ab973d7ad2416eef70ffcbaf6c8122b9bcc455
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-03-14 15:10:36 +0530

    todoSlice Update

--- 08toDoUsingRedux/src/features/todo/todoSlice.js
+++ 08toDoUsingRedux/src/features/todo/todoSlice.js
@@ -10,9 +10,26 @@ const initialState = {
 };
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: ()
-    }
-})
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    // updateTodo: (state, action) => {
+    //   state.todos = state.todos.map((todo) => {
+    //     if (todo.id === action.payload) todo.text = action.payload;
+    //   });
+    // },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
