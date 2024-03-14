9c7f49715bebfafac041b4852a132aa72e6bcb58
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-03-14 13:55:35 +0530

    Redux Toolkit

---
+++ 08toDoUsingRedux/src/features/todo/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "Hello World",
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: ()
+    }
+})
