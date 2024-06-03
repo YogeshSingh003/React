593f42dc96977e9f5edfc3e2e09b7786b444f307
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-06-03 23:04:39 +0530

    Layout Update

--- 05reactRouter/src/Layout.jsx
+++ 05reactRouter/src/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
 import Footer from "./components/Footer";
 
 function Layout() {
